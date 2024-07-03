553766db0ab83217193a2aad60f35e931ad5b9c0
Tom Schultz <2112416+tom-schultz@users.noreply.github.com>
2024-07-03 15:56:07 -0400

    Node configuration warnings. Rad!

--- Attackable.gd
+++ Attackable.gd
@@ -1,7 +1,15 @@
+@tool
 extends Node
 class_name Attackable
 
 signal attacked()
 
+func _process(delta):
+	if Engine.is_editor_hint():
+		update_configuration_warnings()
+
 func attack():
 	attacked.emit()
+
+func _get_configuration_warnings():
+	return [] if unique_name_in_owner and name == "Attackable" else ["Must have the unique name \"Attackable\""]
